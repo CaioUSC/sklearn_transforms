5c67c1a9b94ffb6ffe73c78795d1e4cece0a6808
CaioUSC <Caio@Carmassi.com.br>
2020-08-22 16:02:40 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -18,11 +18,15 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
 
-class SmoteData(object):
-    def __init__(self):
-        pass
+class Setkey(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
 
-    def fit(self, X, y):
-        X_resampled, y_resampled = SMOTE(random_state=42).fit_resample(X, y)
-        X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
-        return X_resampled, y_resampled        
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data.set_index(self.columns, inplace=True)    
